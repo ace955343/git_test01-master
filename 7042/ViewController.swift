facaca286f5b5b32ead9bbad0d60c806687e3121
User21 <user21@user21de-imac.local>
2019-03-21 15:48:29 +0800

    Initial Commit

---
+++ 7042/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7042
+//
+//  Created by User21 on 2019/3/18.
+//  Copyright © 2019 7042. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
